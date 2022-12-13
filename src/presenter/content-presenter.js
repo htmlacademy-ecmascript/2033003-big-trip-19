a36b3a18a5e71a64baaf78b6e832e13ae2d863f9
Alexander <feliugin@gmail.com>
2022-12-13 17:24:13 +0300

    Убрал обьекты которые не надо рендерить по заданию

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,8 +1,6 @@
 import { render } from '../render.js';
 import ContentView from '../view/content-view.js';
 import EditView from '../view/edit-view.js';
-import LoadingView from '../view/loading-view.js';
-import MessageView from '../view/message-view.js';
 import WaypointView from '../view/waypoint-view.js';
 
 export default class ContentPresenter {
@@ -20,7 +18,5 @@ export default class ContentPresenter {
     for(let i = 0; i < this.boardWaypoints.length; i++){
       render(new WaypointView({waypoint: this.boardWaypoints[i]}),this.boardComponent.getElement());
     }
-    render(new LoadingView,this.boardComponent.getElement(),'AFTEREND');
-    render(new MessageView,this.boardComponent.getElement(),'AFTERBEGIN');
   }
 }
