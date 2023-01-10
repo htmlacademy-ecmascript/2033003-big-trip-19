5d492cf729d33c88d740f810cbaa3d840b99fbfe
Alexander <feliugin@gmail.com>
2023-01-10 12:22:12 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,13 @@ import TripInfoView from './view/trip-info.js';
 import ContentPresenter from './presenter/content-presenter.js';
 import {render} from './render.js';
 import SortContainerView from './view/sort-container-view.js';
-import WaypointModel from './model/waypoint-model.js';
 import { SORTINGS } from './const.js';
 
 const siteTripMain = document.querySelector('.trip-main');
 const siteFilterEventsElement = document.querySelector('.trip-controls__filters');
 const siteSortEventsElement = document.querySelector('.trip-events');
 
-const waypointModel = new WaypointModel();
-const contentPresenter = new ContentPresenter({contentContainer: siteSortEventsElement, filtersContainer: siteFilterEventsElement, waypointModel});
+const contentPresenter = new ContentPresenter({contentContainer: siteSortEventsElement, filtersContainer: siteFilterEventsElement});
 
 render(new TripInfoView(), siteTripMain,'AFTERBEGIN');
 render(new SortContainerView({sortings: SORTINGS}),siteSortEventsElement);
